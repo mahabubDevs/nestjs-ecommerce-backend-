9fb3ea2c60598e6bf1385c4446d69d3d16c5f974
mahabubDevs <mdmahabubrahman800@gmail.com>
2025-03-09 01:28:14 +0600

    httpExceptionalFilter Error handel add

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,22 @@
-import { Module } from '@nestjs/common';
+import {  Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import config from 'config'
+import {AllExceptionalFilter} from './httpExcepsonalFilter'
+import { APP_FILTER } from '@nestjs/core';
 
 @Module({
   imports: [
     MongooseModule.forRoot(config.get('mongoURI')),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionalFilter
+    }
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import config from 'config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(config.get('port'), () => {
+    return console.log(`Server is running on port ${config.get('port')}`);
+  } );
 }
 bootstrap();
