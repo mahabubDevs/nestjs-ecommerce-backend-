8c04422292e5ae01c4175c34dd8ba2241f009155
mahabubDevs <mdmahabubrahman800@gmail.com>
2025-03-09 22:59:17 +0600

    error and respons globaly set done

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import config from 'config';
+import { ResponseInterceptor } from './responseInterceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(config.get('port'), () => {
     return console.log(`Server is running on port ${config.get('port')}`);
   } );
